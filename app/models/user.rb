64caea99e0346d1b7150df020fd2af89ba8e557f
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2006-06-14 22:44:56 +0000

    fixed a typo
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/audiobank/trunk@575 20e14386-5fff-0310-853f-b84fc43df78c

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	validates_presence_of :username, :message => "Un nom d'utilisateur est requis"
 	validates_presence_of :name, :message => "Votre nom est requis"
   validates_presence_of :password, :message => "Un mot de passe est requis"
-	validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/, :message => "Un email valide pour vous contactez est requis"
+	validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/, :message => "Un email valide pour vous contacter est requis"
 	
 	def password=(password)
 		write_attribute(:password, Digest::SHA256.hexdigest(password)) unless password.empty?
