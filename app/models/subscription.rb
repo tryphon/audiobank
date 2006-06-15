6af2506722d7076ee5004428fbc2e6e495ce4efe
tim <tim@20e14386-5fff-0310-853f-b84fc43df78c>
2006-06-15 13:39:46 +0000

    check if subscription doesn't already exists
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/audiobank/trunk@585 20e14386-5fff-0310-853f-b84fc43df78c

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,6 @@ class Subscription < ActiveRecord::Base
 	belongs_to :author
 	belongs_to :subscriber
 	belongs_to :document
+	
+	validates_uniqueness_of :subscriber_id, :scope => [:document_id]
 end
