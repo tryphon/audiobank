f5e5866930bb13e034a6cd1f17f6202a483c1ad1
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2006-07-11 22:49:46 +0000

    [Bug 123] create Cue.update
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/audiobank/trunk@730 20e14386-5fff-0310-853f-b84fc43df78c

--- app/models/cue.rb
+++ app/models/cue.rb
@@ -1,12 +1,24 @@
 class Cue < ActiveRecord::Base
 	belongs_to :document
 	
-	def update_file(document)
+	def update_file
+		puts "DEBUG: create cue #{path} with #{document.path}"
 		system "#{RAILS_ROOT}/bin/cueenc", document.path, path
 	end
 
 	def path
 		"#{RAILS_ROOT}/media/cue/#{id}"
+	end
+	
+	def self.update
+		Document.find(:all).delete_if { |d| !d.uploaded? or !d.cues.empty? }.each do |document|
+			puts "INFO: create cue for #{document.id}"
+			cue = Cue.create(:document => document)
+			if !cue.update_file then
+				puts "ERROR: can't create cue for #{document.id}"
+				cue.destroy 
+			end
+		end		
 	end
 	
 end
