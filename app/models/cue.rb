b05df4a476087c1b39d441ecfc81fbb595a0a028
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2006-07-12 06:42:14 +0000

    [Bug 123] create bin/cron-cues
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/audiobank/trunk@731 20e14386-5fff-0310-853f-b84fc43df78c

--- app/models/cue.rb
+++ app/models/cue.rb
@@ -2,7 +2,6 @@ class Cue < ActiveRecord::Base
 	belongs_to :document
 	
 	def update_file
-		puts "DEBUG: create cue #{path} with #{document.path}"
 		system "#{RAILS_ROOT}/bin/cueenc", document.path, path
 	end
 
@@ -12,10 +11,10 @@ class Cue < ActiveRecord::Base
 	
 	def self.update
 		Document.find(:all).delete_if { |d| !d.uploaded? or !d.cues.empty? }.each do |document|
-			puts "INFO: create cue for #{document.id}"
+			puts "INFO: create cue for document #{document.id}"
 			cue = Cue.create(:document => document)
 			if !cue.update_file then
-				puts "ERROR: can't create cue for #{document.id}"
+				puts "ERROR: can't create cue for document #{document.id}"
 				cue.destroy 
 			end
 		end		
