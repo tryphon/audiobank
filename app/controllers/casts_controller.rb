b0f4039bec85455cfe0a22996877b65577d98f14
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2006-07-15 14:05:08 +0000

    fix content type of playcontent
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/audiobank/trunk@747 20e14386-5fff-0310-853f-b84fc43df78c

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -11,14 +11,14 @@ class CastsController < ApplicationController
   
   def playcontent
   	@cast = Cast.find_by_name(params[:name])
-  	send_file @cast.path, :type => "ogg", :filename => "#{@cast.name}"
+  	send_file @cast.path, :type => "application/ogg", :filename => "audiobank-#{@cast.name}.ogg"
   end
 
   def playlist
   	@cast = Cast.find_by_name(params[:name])
   	content = "#EXTM3U\n"
   	content += "#EXTINF:#{@cast.document.length},#{@cast.document.title}\n" 
-  	content += url_for(:action => 'play', :name => @cast.name, :content => 'true')
+  	content += url_for(:action => 'play', :name => @cast.name, :content => 'true') + "\n"
 		render :text => content, :content_type => 'audio/x-mpegurl'
   end
 
--- app/controllers/cues_controller.rb
+++ app/controllers/cues_controller.rb
@@ -5,12 +5,12 @@ class CuesController < ApplicationController
   	@cue = Cue.find(params[:id])
   	content = "#EXTM3U\n"
   	content += "#EXTINF:#{@cue.document.length},#{@cue.document.title}\n" 
-  	content += url_for(:action => 'play', :id => @cue.id)
+  	content += url_for(:action => 'play', :id => @cue.id) + "\n"
 		render :text => content, :content_type => 'audio/x-mpegurl'
   end
   
   def play
   	@cue = Cue.find(params[:id])
-  	send_file @cue.path, :type => "ogg", :filename => "#{@cue.id}"
+  	send_file @cue.path, :type => "application/ogg", :filename => "audiobank-#{@cue.id}.ogg"
   end
 end
