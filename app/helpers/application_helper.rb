0990c85188e2a02e6a5b3d31e0d78069c7133de3
tim <tim@20e14386-5fff-0310-853f-b84fc43df78c>
2006-07-18 21:57:01 +0000

    use error_messages_for helper
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/audiobank/trunk@759 20e14386-5fff-0310-853f-b84fc43df78c

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,18 @@ module ApplicationHelper
 	def format_time(duration)
 		duration.strftime("%H:%M:%S")
 	end
+	
+	def error_messages_for(object_name, options = {})
+		options = options.symbolize_keys
+		object = instance_variable_get("@#{object_name}")
+		if object && !object.errors.empty?
+			content_tag("div",
+				content_tag(options[:header_tag] || "h2", "#{pluralize(object.errors.count, "erreur a été trouvé", "erreurs ont été trouvées")}") +
+					content_tag("ul", object.errors.collect { |name, message| content_tag("li", link_to(message, "##{object_name}_#{name}")) }),
+				"id" => options[:id] || "errors", "class" => options[:class] || "errors"
+			)
+		else
+			""
+		end
+	end
 end
