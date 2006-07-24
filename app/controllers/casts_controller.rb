d7826154dbd723dc623977a4f0fc9e63a6c30388
tim <tim@20e14386-5fff-0310-853f-b84fc43df78c>
2006-07-24 21:06:19 +0000

    should work with xmms and winamp
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/audiobank/trunk@766 20e14386-5fff-0310-853f-b84fc43df78c

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -1,25 +1,26 @@
 class CastsController < ApplicationController
-	layout 'documents', :except => [:play,:playlist]
+	layout nil
 
   def play
-  	if params[:content]
-  		playcontent
+  	name = File.basename(params[:name], ".ogg")
+  	if params[:name] != name
+  		playcontent(name)
   	else 
-  		playlist 
+  		playlist(params[:name])
   	end
   end
   
-  def playcontent
-  	@cast = Cast.find_by_name(params[:name])
-  	send_file @cast.path, :type => "application/ogg", :filename => "audiobank-#{@cast.name}.ogg"
+  private
+  def playcontent(name)
+  	@cast = Cast.find_by_name(name)
+  	send_file @cast.path, :type => "application/ogg", :filename => "audiobank-#{@cast.name}.ogg", :disposition => "inline"
   end
 
-  def playlist
-  	@cast = Cast.find_by_name(params[:name])
+  def playlist(name)
+  	@cast = Cast.find_by_name(name)
   	content = "#EXTM3U\n"
   	content += "#EXTINF:#{@cast.document.length},#{@cast.document.title}\n" 
-  	content += url_for(:action => 'play', :name => @cast.name, :content => 'true') + "\n"
+  	content += url_for(:action => 'play', :name => @cast.name) + ".ogg\n"
 		render :text => content, :content_type => 'audio/x-mpegurl'
   end
-
 end
