5146d472e2f2ce976bf9b92d74d47f46ed66d392
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2007-06-02 13:44:09 +0000

    freeze openid and yadis libraries
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/audiobank/trunk@1105 20e14386-5fff-0310-853f-b84fc43df78c

--- vendor/openid/examples/rails_server/config/environment.rb
+++ vendor/openid/examples/rails_server/config/environment.rb
@@ -15,20 +15,18 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  config.load_paths += %W( #{RAILS_ROOT}/vendor/openid/lib #{RAILS_ROOT}/vendor/yadis/lib )
 
   # Force all environments to use the same logger level 
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
 
   # Use the database for sessions instead of the file system
-  # (create the session table with 'rake db:sessions:create')
-  config.action_controller.session_store = :active_record_store
+  # (create the session table with 'rake create_sessions_table')
+  # config.action_controller.session_store = :active_record_store
 
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper, 
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
+  # Enable page/fragment caching by setting a file-based store
+  # (remember to create the caching directory and make it readable to the application)
+  # config.action_controller.fragment_cache_store = :file_store, "#{RAILS_ROOT}/cache"
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
@@ -36,6 +34,10 @@ Rails::Initializer.run do |config|
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
   
+  # Use Active Record's schema dumper instead of SQL when creating the test database
+  # (enables use of different database adapters for development and test environments)
+  # config.active_record.schema_format = :ruby
+
   # See Rails::Configuration for more options
 end
 
@@ -49,4 +51,4 @@ end
 # end
 
 # Include your application configuration below
-require 'custom.rb'
+ActionController::CgiRequest::DEFAULT_SESSION_OPTIONS[:session_key] = '_session_id_2'
