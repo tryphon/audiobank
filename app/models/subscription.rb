a379f605f51c1065ef400fd26b7cab62a97c2e5c
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2008-07-12 00:09:46 +0000

    Filtre les Subscriptions sans User et les marque notified. Fixes #17
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/audiobank/trunk@1192 20e14386-5fff-0310-853f-b84fc43df78c

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,31 +2,41 @@ class Subscription < ActiveRecord::Base
 	belongs_to :author, :class_name => "User", :foreign_key => "author_id"
 	belongs_to :subscriber, :polymorphic => true
 	belongs_to :document
-	
+
 	validates_uniqueness_of :subscriber_id, :scope => [:document_id, :subscriber_type]
-	
+
 	def self.notify
 	  unnotified_subscriptions = Subscription.find_all_by_notified(false)
+
+	  unnotified_subscriptions.delete_if do |subscription|
+  	  if subscription.users.empty?
+  	    subscription.update_attribute(:notified, true)
+  	    true
+  	  else
+  	    false
+      end
+	  end
+
 	  puts "#{unnotified_subscriptions.size} unnotified subscriptions" unless unnotified_subscriptions.empty?
-	  
+
 	  mapped_subscriptions = Hash.new { |hash, user| hash[user] = Array.new }
-	  
+
 	  unnotified_subscriptions.inject(mapped_subscriptions) do |map, subscription|
-	    subscription.users.each do |user| 
+	    subscription.users.each do |user|
 	      map[user].push(subscription)
 	    end
   	  map
 	  end
-	  
+
 	  mapped_subscriptions.each do |user, subscriptions|
 	    puts "notify #{user.username} for #{subscriptions.size} subscription(s)"
-	    Mailer.deliver_document_shared(user, subscriptions)  
+	    Mailer.deliver_document_shared(user, subscriptions)
 	    subscriptions.each { |subscription| subscription.update_attribute("notified", true) }
 	  end
 	end
-	
+
 	def users
 	 subscriber.is_a?(Group) ? subscriber.users : [ subscriber ]
 	end
-	
+
 end
