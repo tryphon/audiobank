c3e574f68be3881b5d7aff704c9ef5817e0b9c84
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2008-08-25 12:41:18 +0000

    Valide de Podcast#title est defini. Fixes #18
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/audiobank/trunk@1363 20e14386-5fff-0310-853f-b84fc43df78c

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -2,6 +2,8 @@ class Podcast < ActiveRecord::Base
 	has_and_belongs_to_many :tags
 	belongs_to :author, :class_name => "User", :foreign_key => "author_id"
 
+	validates_presence_of :title, :message => "Le titre ne peut être vide"
+
   def tag_with(list)
   	Tag.transaction do
   		self.tags = Tag.parse(list)
