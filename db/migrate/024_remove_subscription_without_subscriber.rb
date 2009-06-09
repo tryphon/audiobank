bdd2fe01deed4f169a7f15d2fd9c14f8c93cb98b
Alban Peignier <alban.peignier@free.fr>
2009-06-09 09:09:10 +0200

    Remove a depricated find_all

--- db/migrate/024_remove_subscription_without_subscriber.rb
+++ db/migrate/024_remove_subscription_without_subscriber.rb
@@ -1,6 +1,6 @@
 class RemoveSubscriptionWithoutSubscriber < ActiveRecord::Migration
   def self.up
-    Subscription.find_all.delete_if { |s| ! s.subscriber.nil? }.each do |s|
+    Subscription.find(:all).delete_if { |s| ! s.subscriber.nil? }.each do |s|
       puts "destroy subscription #{s.id} (no subscriber)"
       s.destroy
     end
