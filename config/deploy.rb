ca0a77c8dc890b1e0bf44b009780aa8289f91030
Alban Peignier <alban@tryphon.eu>
2011-08-09 17:34:49 +0200

    Change git repository

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,17 @@
 set :application, "audiobank"
 set :scm, "git"
-set :repository, "git://www.dbx.tryphon.priv/audiobank"
+set :repository, "git://www.tryphon.priv/audiobank"
 
 set :deploy_to, "/var/www/audiobank"
 
 set :keep_releases, 5
 after "deploy:update", "deploy:cleanup" 
 set :use_sudo, false
+default_run_options[:pty] = true
 
-# server "radio.dbx.tryphon.priv", :app, :web, :db, :primary => true
-server "sandbox", :app, :web, :db, :primary => true
+set :rake, "bundle exec rake"
+
+server "radio.dbx1.tryphon.priv", :app, :web, :db, :primary => true
 
 after "deploy:update_code", "deploy:symlink_shared", "deploy:gems"
 
@@ -39,11 +41,11 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/config/production.rb #{release_path}/config/environments/"
 
     run "ln -nfs #{shared_path}/media #{release_path}/media"
-    run "ln -nfs #{shared_path}/casts #{release_path}/media/casts"
-    run "ln -nfs #{shared_path}/upload #{release_path}/media/upload"
+    sudo "ln -nfs #{shared_path}/casts #{release_path}/media/casts"
+    sudo "ln -nfs #{shared_path}/upload #{release_path}/media/upload"
     run "ln -nfs #{shared_path}/playlists #{release_path}/public/cache"
 
     # FIXME remove media/cast
-    run "ln -nfs #{shared_path}/casts #{release_path}/media/cast"
+    sudo "ln -nfs #{shared_path}/casts #{release_path}/media/cast"
   end
 end
