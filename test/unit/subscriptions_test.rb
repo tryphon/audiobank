c6c6153b4ec8ad2f704c12ae40cef84fa3f53dd5
Alban Peignier <alban@tryphon.eu>
2011-09-19 20:15:10 +0200

    Remove oldies unit tests

--- test/unit/subscriptions_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-
-class SubscriptionsTest < Test::Unit::TestCase
-  fixtures :subscriptions
-
-  def test_truth
-    assert_kind_of Subscription, Subscription.find(:first)
-  end
-end
