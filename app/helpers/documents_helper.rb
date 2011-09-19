000ee1edf70df653458e86dc1997f0ef55674dfc
Alban Peignier <alban@tryphon.eu>
2011-09-19 20:17:27 +0200

    Display the correct cast url

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,8 @@
 module DocumentsHelper
+
+  def public_cast_url(document)
+    url_for :controller => 'casts', :action => 'play', :name => document.casts.first.name, :only_path => false
+  end
+
+
 end
