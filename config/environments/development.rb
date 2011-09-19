e33cd112ca673fae4476df373c88766adcb61c3f
Alban Peignier <alban@tryphon.eu>
2011-09-19 20:17:49 +0200

    Set smtp_settings for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,7 @@ config.action_view.debug_rjs                         = true
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+ActionMailer::Base.smtp_settings = {
+  :address        => "mail.tryphon.priv"
+}
