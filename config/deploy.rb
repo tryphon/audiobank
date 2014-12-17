90b83952350dbb6ad7c359611ad17ca704766f50
Alban Peignier <alban@tryphon.eu>
2014-12-17 19:33:50 +0100

    Add symbolic link for newrelic config

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ namespace :deploy do
 
   desc "Symlinks shared configs and folders on each release"
   task :symlink_shared, :except => { :no_release => true }  do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/"
+    run "ln -nfs #{shared_path}/config/database.yml #{shared_path}/config/newrelic.yml #{release_path}/config/"
     run "ln -nfs #{shared_path}/config/production.rb #{release_path}/config/environments/"
 
     run "ln -nfs #{shared_path}/media #{release_path}/media"
