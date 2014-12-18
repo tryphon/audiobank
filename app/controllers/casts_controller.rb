61f4b7cabe7fa078bf2e68deef2ffd7ed406d906
Alban Peignier <alban@tryphon.eu>
2014-12-18 14:04:39 +0100

    Add json response in CastsController. Refactor m3u response (without static file). Refs #1273

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -4,63 +4,37 @@ class CastsController < ApplicationController
   skip_before_filter :check_authentication
 
   def play
-  	case params[:format]
-  	when "mp3"
-  		playcontent(File.basename(params[:name], "."+ params[:format]), params[:format])
-  	when "ogg"
-  		playcontent(File.basename(params[:name], "."+ params[:format]), params[:format])
+    format = params[:format]
+    name = params[:name]
+
+  	@cast = Cast.find_by_name!(name)
+
+  	case format
+  	when "mp3", "ogg"
+  		playcontent @cast, format
   	when "m3u"
-  		playlist(File.basename(params[:name], "."+ params[:format]))
+  		render text: playlist(@cast), type: "audio/x-mpegurl"
+    when "json"
+      render json: { title: @cast.document.title, duration: @cast.document.duration, tags: @cast.document.tags.map(&:name) }
   	else
-  		redirect_to :action => :play, :name => params[:name]+".m3u"
+  		redirect_to :action => :play, :name => "#{name}.m3u"
   	end
   end
 
   private
-  def player?
-  	if request.env['HTTP_USER_AGENT'] =~ /^Winamp|Windows/
-  		true
-  	else
-  		false
-  	end
-  end
 
-  def mp3_only_player?
-  	if request.env['HTTP_USER_AGENT'] =~ /^Windows|mpg321|RMA|iTunes/
-  		true
-  	else
-  		false
-  	end
-  end
-
-  def playcontent(name, format)
-  	@cast = Cast.find_by_name(name)
-    raise ActiveRecord::RecordNotFound unless @cast
-
+  def playcontent(cast, format)
     unless request.head?
-      @cast.increment(:download_count).save
-      logger.info "Play Cast #{@cast.name} #{format} #{@cast.size(format)} #{@cast.download_count} #{@cast.document.id} #{@cast.document.author.username} \"#{@cast.document.title}\""
+      cast.increment(:download_count).save
+      logger.info "Play Cast #{cast.name} #{format} #{cast.size(format)} #{cast.download_count} #{cast.document.id} #{cast.document.author.username} \"#{cast.document.title}\""
     end
 
-  	redirect_to "/static/cast/#{@cast.filename(format)}"
+  	redirect_to "/static/cast/#{cast.filename(format)}"
   end
 
-  def playlist(name)
-  	@cast = Cast.find_by_name(name)
-    raise ActiveRecord::RecordNotFound unless @cast
-
+  def playlist(cast)
   	content = "#EXTM3U\n"
-  	content += "#EXTINF:#{@cast.document.length},#{@cast.document.title}\n"
-  	content += url_for(:action => 'play', :name => @cast.name) + ".mp3\n"
-
-		m3u_url = "cache/#{name}.m3u"
-		m3u_path = "#{Rails.root}/public/#{m3u_url}"
-		unless File.exists?(m3u_path)
-			File.open(m3u_path, "w") do |f|
-					f.write(content)
-			end
-		end
-
-  	redirect_to "/#{m3u_url}"
+  	content += "#EXTINF:#{cast.document.length},#{cast.document.title}\n"
+  	content += url_for(:action => 'play', :name => cast.name) + ".mp3\n"
   end
 end
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,9 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+Mime::Type.register "audio/ogg", :ogg
+Mime::Type.register "audio/x-flac", :flac
+Mime::Type.register "audio/x-wav", :wav
+Mime::Type.register 'audio/mpeg', :mp3
+Mime::Type.register_alias "audio/x-mpegurl", :m3u
