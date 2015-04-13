905d1a8a509a291970da42b2ae3f4513127c8ed6
Alban Peignier <alban@tryphon.eu>
2015-04-13 11:41:21 +0200

    Simplify AddUserToDownloads migration for SQLite database. Refs #1380

--- db/migrate/20150413085822_add_user_to_downloads.rb
+++ db/migrate/20150413085822_add_user_to_downloads.rb
@@ -1,7 +1,11 @@
 class AddUserToDownloads < ActiveRecord::Migration
   def change
     add_column :downloads, :user_id, :integer
-    Download.connection.execute "update downloads, documents set downloads.user_id = documents.author_id where downloads.document_id = documents.id;"
+
+    unless Download.connection.adapter_name == 'SQLite'
+      Download.connection.execute "update downloads, documents set downloads.user_id = documents.author_id where downloads.document_id = documents.id;"
+    end
+
     add_index :downloads, :user_id
   end
 end
